efd8e425ac2d30fc758a38be8bb3a24dfaac5345
Chris Vombaur <cvombaur@gmail.com>
2021-10-20 21:56:36 -0600

    initial

---
+++ index.js
@@ -0,0 +1,55 @@
+
+var button = document.querySelector('.increase-btn');
+var counter = document.querySelector('.counter');
+
+button.addEventListener('click', function(){
+  counter.value = parseInt(counter.value) + 100; 
+});
+
+var button = document.querySelector('.decrease-btn');
+var counter = document.querySelector('.counter');
+
+button.addEventListener('click', function(){
+  counter.value = parseInt(counter.value) - 100; 
+});
+
+document.querySelector("form").addEventListener("submit", addPlayer);
+const message = document.querySelector('#message')
+
+function addPlayer(event){
+  event.preventDefault();
+  let inputField = document.querySelector('input')
+  
+  
+  const player = document.createElement('li')
+
+  const playerName = document.createElement("span");
+  playerName.textContent = inputField.value;
+  player.appendChild(playerName)
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", deletePlayer);
+  player.appendChild(deleteBtn);
+  
+
+  const list = document.querySelector("ul");
+  list.appendChild(player);
+
+  inputField.value = ''
+}
+function deletePlayer(event) {
+     event.target.parentNode.remove();
+}
+function revealMessage() {
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
+}
+
+
+
+
+
+
+
